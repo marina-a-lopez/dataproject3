436e20e847a9dc7af19ed7a76c116da5598575ca
marina-a-lopez <malope@edem.es>
2026-05-08 16:44:45 +0200

    elimino apy key

--- terraform/main.tf
+++ terraform/main.tf
@@ -183,15 +183,6 @@ resource "google_cloud_run_v2_service" "backend_cloud_run" {
         }
       }
       env {
-        name  = "GEMINI_API_KEY"
-        value_source {
-          secret_key_ref {
-            secret  = google_secret_manager_secret.gemini_api_key.secret_id
-            version = "latest"
-          }
-        }
-      }
-      env {
         name  = "GCP_PROJECT_ID"
         value = var.project_id
       }
