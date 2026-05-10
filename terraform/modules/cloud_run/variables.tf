36337ebaaea17c505715ddc56cfb91510ec28ad8
marina-a-lopez <malope@edem.es>
2026-05-10 17:20:31 +0200

    cicd2

--- terraform/modules/cloud_run/variables.tf
+++ terraform/modules/cloud_run/variables.tf
@@ -45,17 +45,17 @@ variable "rag_corpus_id_secret_id" {
 variable "backend_image" {
   description = "Imagen Docker del backend (gestionada por CI/CD)"
   type        = string
-  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/backend:latest"
+  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/backend:c3e6ae7f121037a11f2915da5d1de735f63e56bf"
 }
 
 variable "frontend_image" {
   description = "Imagen Docker del frontend web (gestionada por CI/CD)"
   type        = string
-  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/frontend:latest"
+  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/frontend:d253468b9f73c0f75abc4e9d8204f8d910351476"
 }
 
 variable "dashboard_image" {
   description = "Imagen Docker del dashboard de inversores (gestionada por CI/CD)"
   type        = string
-  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/dashboard:latest"
+  default     = "europe-southwest1-docker.pkg.dev/proyectodataia3/repo-aitonomo/dashboard:a916c795429475ab9f41f248941e36c1e5e9b73a"
 }
