897fa345c4dba34328b40fe88d23196792ad2b1c
marina-a-lopez <malope@edem.es>
2026-05-10 20:06:09 +0200

    Update main.tf

--- terraform/modules/storage/main.tf
+++ terraform/modules/storage/main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "document_bucket" {
-  name          ="bucket-aitonomo-docs"
+  name          = "bucket-aitonomo-docs"
   location      = var.region
   force_destroy = false
 }
